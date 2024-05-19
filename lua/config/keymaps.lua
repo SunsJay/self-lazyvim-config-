8dfcfae3d2f1eff72f720451067edc878e1ac047
Suns Jay <sunsjay0806@gmail.com>
2024-05-19 14:38:13 +0800

    feat: add ctrl e/a keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,8 @@
 --
 
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
 
 keymap.set({ "n", "x" }, "x", '"_x', { desc = "删除" })
+keymap.set("n", "<leader>up", "<Esc>viwUea<Esc>", { noremap = true, silent = true, desc = "切换单词大小写" })
+keymap.set("i", "<C-e>", "<end>", { desc = "光标移动到行尾", noremap = true, silent = true })
+keymap.set("i", "<C-a>", "<home>", { noremap = true, silent = true, desc = "光标移动到行首" })
